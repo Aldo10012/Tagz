1502136169b30def8365d5b6808e475de2536da3
Alberto <aldo10012@gmail.com>
2021-11-24 14:51:53 -0800

    initialized project

---
+++ Tagz/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Tagz
+//
+//  Created by Alberto Dominguez on 11/24/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+}

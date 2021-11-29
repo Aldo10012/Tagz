1326d5d4866fe41c2f342323d973f677261e7c6b
Alberto <aldo10012@gmail.com>
2021-11-29 15:10:30 -0800

    added chase functionality to AI

--- Tagz/GameScene.swift
+++ Tagz/GameScene.swift
@@ -15,12 +15,15 @@ class GameScene: SKScene {
     // MARK: Properties
     
     // Nodes
-    var playerNode = SKShapeNode()
-    var enemyNode = SKShapeNode()
+    var playerNode = SKSpriteNode()
+    var enemyNode = SKSpriteNode()
+    
+    // time intervals
+    let fixedDelta: CFTimeInterval = 1.0 / 60.0 /* 60 FPS */
     
     // Speed
-    var playerSpeed = CGFloat(0.05)
-    var enemySpeed = CGFloat(0.04)
+    var playerSpeed: CGFloat = 1
+    var enemySpeed: CGFloat = 0.75
     
     // joystick controller
     var analogJoystick: TLAnalogJoystick = {
@@ -36,15 +39,19 @@ class GameScene: SKScene {
     
     // MARK: Life Cycle
     override func didMove(to view: SKView) {
-        playerNode = self.childNode(withName: "player") as! SKShapeNode
-        enemyNode = self.childNode(withName: "enemy") as! SKShapeNode
+        playerNode = self.childNode(withName: "player") as! SKSpriteNode
+        enemyNode = self.childNode(withName: "enemy") as! SKSpriteNode
         
+        physicsWorld.contactDelegate = self
+        
+        configurePhysicsBody()
         setupJoyStick()
     }
     
     override func update(_ currentTime: TimeInterval) {
         rotateEnemyNodeToFacePlayerNode()
         moveEnemyToPLayer()
+        
     }
     
     // MARK: Helpers
@@ -56,40 +63,112 @@ class GameScene: SKScene {
             let pVelocity = joystick.velocity;
             let speed = playerSpeed
             
-            playerNode.position = CGPoint(x: playerNode.position.x + (pVelocity.x * speed),
-                                          y: playerNode.position.y + (pVelocity.y * speed))
+            playerNode.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            playerNode.physicsBody?.applyImpulse(CGVector(dx: pVelocity.x * playerSpeed,
+                                                          dy: pVelocity.y * playerSpeed))
             playerNode.zRotation = joystick.angular
         }
+        
+        analogJoystick.on(.end) { [unowned self] joystick in
+            playerNode.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        }
+        
     }
     
     func rotateEnemyNodeToFacePlayerNode() {
         let dy = playerNode.position.y - enemyNode.position.y
         let dx = playerNode.position.x - enemyNode.position.x
-        let angle = Trig().cot(rise: dy, run: dx)
+        let angle = atan2(dy, dx) //Trig().cot(rise: dy, run: dx)
         enemyNode.zRotation = angle
     }
     
     func moveEnemyToPLayer() {
         let dx = playerNode.position.x - enemyNode.position.x
-        let dy = playerNode.position.y  - enemyNode.position.y
+        let dy = playerNode.position.y - enemyNode.position.y
         
-        let enemySpeed = sqrt((dx * dx) + (dy * dy))
+        enemyNode.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
         
-        // TODO: not using enemySpeed set from Properties. Will make it hard to adjust enemy speed when choosing difficulty.
-        enemyNode.position = CGPoint(x: enemyNode.position.x + (dx/enemySpeed),
-                                     y: enemyNode.position.y + (dy/enemySpeed))
+        let enemyImpulse = setEnemyImpulse(dx: dx, dy: dy)
+        enemyNode.physicsBody?.applyImpulse(enemyImpulse)
     }
     
+    
+    /// recursive function to set enemy Vector
+    func setEnemyImpulse(dx: CGFloat, dy: CGFloat) -> CGVector {
+        let dx = dx
+        let dy = dy
+        let hyp = sqrt((dx * dx) + (dy * dy))
+        // hyp should always be 100
+        
+        let lowerBound: CGFloat = 48 * enemySpeed
+        let upperBound: CGFloat = 52 * enemySpeed
+        
+        
+        // BASE CASE: if hyp is within 2 of 100, return
+        if lowerBound < hyp && hyp < upperBound {
+            return CGVector(dx: dx, dy: dy)
+        }
+        else if hyp < lowerBound {
+            return setEnemyImpulse(dx: dx*1.5, dy: dy*1.5)
+        }
+        else  {  // if hyp > upperBound
+            return setEnemyImpulse(dx: dx/2, dy: dy/2)
+        }
+        
+    }
     
     
 }
 
+// MARK: Physics Bodies
+extension GameScene {
+    func configurePhysicsBody() {
+        let boarder = SKPhysicsBody(edgeLoopFrom: self.frame)
+        boarder.friction = 0
+        boarder.restitution = 0
+        
+        self.physicsBody = boarder
+        
+        // Setting up edge Physics
+        self.physicsBody?.categoryBitMask = PhysicsCatagory.Edge
+        self.physicsBody?.collisionBitMask = PhysicsCatagory.Player | PhysicsCatagory.Enemy
+        self.physicsBody?.contactTestBitMask = PhysicsCatagory.Player | PhysicsCatagory.Enemy
+        
+        // set up player physics
+        playerNode.physicsBody?.categoryBitMask = PhysicsCatagory.Player
+        playerNode.physicsBody?.collisionBitMask = PhysicsCatagory.Enemy | PhysicsCatagory.Edge | PhysicsCatagory.Obstacle
+        playerNode.physicsBody?.contactTestBitMask = PhysicsCatagory.Enemy  | PhysicsCatagory.Edge  | PhysicsCatagory.Obstacle
+        
+        // set up enemy physics
+        enemyNode.physicsBody?.categoryBitMask = PhysicsCatagory.Enemy
+        enemyNode.physicsBody?.collisionBitMask = PhysicsCatagory.Player | PhysicsCatagory.Edge | PhysicsCatagory.Obstacle
+        enemyNode.physicsBody?.contactTestBitMask = PhysicsCatagory.Player | PhysicsCatagory.Edge | PhysicsCatagory.Obstacle
+        
+        // set up obstacle physics
+    }
+}
+
+// MARK: SKPhysicsContactDelegate
+extension GameScene: SKPhysicsContactDelegate {
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        print("contact")
+    }
+    
+}
 
 
+// MARK: Physics Model
 
+struct PhysicsCatagory {
+    static let None:       UInt32 = 0        // 0000000 0
+    static let Player:     UInt32 = 0b1      // 0000001 1
+    static let Enemy:      UInt32 = 0b10     // 0000010 2
+    static let Obstacle:   UInt32 = 0b100    // 0000100 4
+    static let Edge:       UInt32 = 0b1000   // 0001000 8
+}
 
-
-
+// MARK: ScreenSize
 struct ScreenSize {
     static let width  = UIScreen.main.bounds.width
     static let height = UIScreen.main.bounds.height
@@ -97,21 +176,3 @@ struct ScreenSize {
     static let minLength = min(ScreenSize.width, ScreenSize.height)
 }
 
-
-struct Trig {
-    /*
-    sin(Θ) = rise/hyp
-    cos(Θ) = run /hyp
-    tan(Θ) = rise/run
-    
-    csc(rise/hyp) = Θ
-    sec(run/hyp)  = Θ
-    cot(rise/hyp) = Θ
-    */
-    
-    /// finds angle Θ based on rise and run
-    /// angle = cot(rise: playerVelocity.dy, run: playerVelocity.dx)
-    func cot(rise: CGFloat, run: CGFloat) -> CGFloat {
-        return atan2(rise, run)
-    }
-}
